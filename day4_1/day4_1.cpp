abd98c6454ed06ecc99dacd9c77e398932969fd8
Mattia Scarpa <scarpa.mat25@gmail.com>
2021-12-05 18:01:37 +0100

    day4_1

---
+++ day4_1/day4_1.cpp
@@ -0,0 +1,100 @@
+// day4_1.cpp : Questo file contiene la funzione 'main', in cui inizia e termina l'esecuzione del programma.
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <fstream>
+#include <stdlib.h>
+
+constexpr auto BOARD_SIZE = 5;
+
+// Creating bingo boards
+
+struct board {
+	int table[5][5]{};
+	int sum = 0;
+	std::vector<int> rows = { std::vector<int>(5,0) };		// marked numbers counter in rows
+	std::vector<int> cols = { std::vector<int>(5,0) };		// marked numbers counter in columns
+};
+
+// Utilities funciton
+
+void extractSequence(std::string sSeq, std::vector<int> &vSeq) {
+	vSeq.clear();
+	std::size_t pos = 0;
+	std::size_t found = sSeq.find(",", pos);
+
+	while (found != sSeq.npos) {
+		vSeq.push_back(stoi(sSeq.substr(pos, found-pos)));
+		pos = found+1;
+		found = sSeq.find(",", pos);
+	}
+	vSeq.push_back(stoi(sSeq.substr(pos, sSeq.length()-pos)));		// extract last value
+}
+
+void fillBoards(std::ifstream &inputFile, std::vector<board> &boards) {
+	int num;
+	while (true) {
+		board bTemp;
+
+		for (size_t i = 0; i < BOARD_SIZE; i++) {
+			for (size_t j = 0; j < BOARD_SIZE; j++) {		// adding line to board
+				if (!(inputFile >> num)) {
+					return;
+				}
+				else {
+					bTemp.table[i][j] = num;
+					bTemp.sum = bTemp.sum + num;
+				}
+			}
+		}
+		boards.push_back(bTemp);
+	}
+}
+
+int main() {
+
+	std::ifstream inputFile;
+	inputFile.open("../inputBingo.txt");
+
+	if (!inputFile) {
+		std::cerr << "Unable to open input file!" << std::endl;
+		exit(1);
+	}
+
+	std::string seq;
+	inputFile >> seq;
+	std::vector<int> sequence;
+	extractSequence(seq, sequence);
+
+	std::vector<board> boards;
+
+	fillBoards(inputFile, boards);
+
+	int winningBoard, last;
+	for (int extraction : sequence) {
+
+		for (size_t b = 0; b < boards.size(); b++) {
+			for (size_t i = 0; i < BOARD_SIZE; i++) {
+				for (size_t j = 0; j < BOARD_SIZE; j++) {
+					if (boards[b].table[i][j] == extraction) {
+						boards[b].sum -= extraction;
+						boards[b].cols[i]++;
+						boards[b].rows[j]++;
+
+						if (boards[b].cols[i] == 5 || boards[b].rows[j] == 5) {
+							winningBoard = boards[b].sum;
+							last = extraction;
+							std::cout << b << std::endl;
+							std::cout << "The winning board has sum " << winningBoard << ".\nThe final score is -> " << winningBoard * last << std::endl;
+							return winningBoard * last;
+							exit(1);
+						}
+					}
+				}
+			}
+		}
+	}
+}
